ac59ea51a4a22474a2c8115c4772aa315de9b8c1
Michel VILLANUEVA <emhernand@fis.cinvestav.mx>
2016-07-29 11:46:02 -0500

    Fixing counting error.

--- get_hepdata.py
+++ get_hepdata.py
@@ -4,7 +4,7 @@ from src.hepsearch import *
 import pickle
 
 # First, read the nodes list
-nodes_file = read_csv('RedFAENodes_tiny.csv')
+nodes_file = read_csv('RedFAENodes.csv')
 nodes_file.pop(0)
 
 num_authors = len(nodes_file)
@@ -17,8 +17,7 @@ Matrix = [[empty_list for x in range(num_authors)] for y in range(num_authors)]
 # Make the search of authors
 for author in nodes_file:
     for author2 in nodes_file:
-        if author[0] < author2[0]:
-
+        if int(author[0]) < int(author2[0]):
             list = make_query_authors(find_signature(author[1]), find_signature(author2[1]))
             Matrix[int(author[0])][int(author2[0])] = list
             Matrix[int(author2[0])][int(author[0])] = list
